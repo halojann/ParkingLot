960594b383e2f36c823c95097131e5faf435fa8f
halojann <alanmmpr@gmail.com>
2017-04-26 15:06:33 -0400

    httpprocess - token

--- operator/src/main/java/temple/edu/operator/token.java
+++ operator/src/main/java/temple/edu/operator/token.java
@@ -1,7 +1,10 @@
 package temple.edu.operator;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -9,7 +12,12 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.DataOutputStream;
 import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.security.InvalidKeyException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
@@ -19,8 +27,9 @@ import java.security.SignatureException;
 import java.security.spec.X509EncodedKeySpec;
 
 public class token extends Activity {
-    String str;
-    JSONObject token=null;
+    String str, server_url = "http://10.109.106.250:8000/accounts/arrive/";
+    JSONObject token = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,18 +40,22 @@ public class token extends Activity {
     private void processintent(Intent intent) throws JSONException {
         intent.getStringExtra("token");
         token = new JSONObject(intent.getStringExtra("token"));
-        if(token!=null){
+        if (token != null) {
             try {
-                String data = token.getString("username")+token.getString("parkinglot")+token.getString("start")+token.getString("end")+token.getString("transaction");
-                byte [] signature = token.getString("signature").getBytes();
-                if(verifySignature(data.getBytes(),signature,readPublicKey())){
-                    Toast.makeText(this,"verification successful",Toast.LENGTH_LONG).show();
+                String data = token.getString("username") + token.getString("parkinglot") + token.getString("start") + token.getString("end") + token.getString("transaction");
+                byte[] signature = token.getString("signature").getBytes();
+                if (verifySignature(data.getBytes(), signature, readPublicKey())) {
+                    Toast.makeText(this, "verification successful", Toast.LENGTH_LONG).show();
 
+                    //notify server of user arrival
+                    JSONObject json = new JSONObject();
+                    json.put("transaction", token.getString("transaction"));
+                    new SendtoServer().execute(server_url, json.toString());
                 }
 
             } catch (JSONException e) {
                 e.printStackTrace();
-                Log.e("token parse:  ","token can't parse");
+                Log.e("token parse:  ", "token can't parse");
             }
         }
     }
@@ -62,7 +75,7 @@ public class token extends Activity {
     }
 
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
         try {
             processintent(getIntent());
@@ -72,20 +85,20 @@ public class token extends Activity {
 
 
     }
-    public PublicKey  readPublicKey(){
-        String res="";
-        try{
+
+    public PublicKey readPublicKey() {
+        String res = "";
+        try {
             FileInputStream fin = openFileInput("public_key");
             int length = fin.available();
-            byte [] buffer = new byte[length];
+            byte[] buffer = new byte[length];
             fin.read(buffer);
             fin.close();
             X509EncodedKeySpec spec =
                     new X509EncodedKeySpec(buffer);
             KeyFactory kf = KeyFactory.getInstance("RSA");
             return kf.generatePublic(spec);
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
@@ -133,7 +146,7 @@ public class token extends Activity {
 //        return PublicKey;
 //    }
 
-    protected Boolean verifySignature(byte[] data,byte[] token,PublicKey publicKey){
+    protected Boolean verifySignature(byte[] data, byte[] token, PublicKey publicKey) {
         Signature s = null;
         boolean valid = false;
         try {
@@ -156,5 +169,68 @@ public class token extends Activity {
             e.printStackTrace();
         }
         return valid;
+    }
+
+    private class SendtoServer extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String data = "";
+            HttpURLConnection httpURLConnection = null;
+            try {
+
+                //connect to url
+                httpURLConnection = (HttpURLConnection) new URL(params[0]).openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+
+
+                //POST JSON.tostring()
+                DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
+                wr.writeBytes("PostData=" + params[1]);
+                wr.flush();
+                wr.close();
+
+                //get response
+                Log.d("Response code: ", (String.valueOf(httpURLConnection.getResponseCode())));
+                InputStream in = httpURLConnection.getInputStream();
+
+                InputStreamReader inputStreamReader = new InputStreamReader(in);
+
+                int inputStreamData = inputStreamReader.read();
+                while (inputStreamData != -1) {
+                    char current = (char) inputStreamData;
+                    inputStreamData = inputStreamReader.read();
+                    data += current;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (httpURLConnection != null) {
+                    httpURLConnection.disconnect();
+                }
+            }
+
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(final String result) {
+            super.onPostExecute(result);
+            Log.d("arrival response: ", result);
+            Toast.makeText(getApplicationContext(), "Arrival Notified", Toast.LENGTH_LONG).show();
+            String status = "";
+
+            try {
+                JSONObject json = new JSONObject(result);
+                status = json.getString("status");
+
+            } catch (JSONException j) {
+                j.printStackTrace();
+                Log.d("json status ", status);
+            }
+
+        }
     }
 }
